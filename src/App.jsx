9ebc410903ed8cc5c11dd0299a5d0e4b98851b3c
mk-7429j <manipkm0004@gmail.com>
2025-01-25 21:58:28 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-full flex items-center justify-start'>
+      <h1 className='text-5xl underline font-bold text-red-500'>Hii ....</h1>
+    </div>
+  )
+}
+
+export default App
